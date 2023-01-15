072caf4ba8d795926ceeaa72d230146a6495abc6
Palli Moon <pallimoon@gmail.com>
2023-01-15 22:16:45 +0000

    Add event queue and event processing on startup on query service

--- mini-microservice/event-bus/index.js
+++ mini-microservice/event-bus/index.js
@@ -5,10 +5,12 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
 
-  console.log(event);
+  events.push(event);
 
   axios
     .post('http://localhost:4000/events', event) // Posts
@@ -24,6 +26,10 @@ app.post('/events', (req, res) => {
     .catch((err) => console.error(err.message));
 
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => console.log('Listening on 4005'));
