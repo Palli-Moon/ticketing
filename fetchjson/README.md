00cd67eb73053bbae515e8733f3e9dd26652bb9e
Palli Moon <pallimoon@gmail.com>
2023-01-19 20:36:41 +0000

    Create small typescript project

---
+++ fetchjson/README.md
@@ -0,0 +1,4 @@
+Requires `npm install -g typescript ts-node` before compiling.
+
+`tsc index.ts` to compile. `node index.js` to run compiled code.
+`ts-node index.ts` to compile and run straight away.
---
+++ fetchjson/index.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/8';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((res) => {
+  const { id, title, completed } = res.data as Todo;
+
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed ? 'yes' : 'no'}
+  `);
+});
