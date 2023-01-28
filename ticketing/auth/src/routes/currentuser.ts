6559b2de56a8b8e77916b750751cdd74ed1be742
Palli Moon <pallimoon@gmail.com>
2023-01-28 14:48:18 +0000

    Implement current user

--- ticketing/auth/src/routes/currentuser.ts
+++ ticketing/auth/src/routes/currentuser.ts
@@ -1,10 +1,22 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 // is there perhaps an easier way than always writing the '/api/users' part?
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there');
+  if (!req.session?.jwt) {
+    // ? is equal to checking !req.session first
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+
+    res.send({ currentUser: payload });
+  } catch (error) {
+    return res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter }; // Rename the exported router
