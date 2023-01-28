a7a02454de73d88a2cf46b5f46904347053d5dd8
Palli Moon <pallimoon@gmail.com>
2023-01-28 13:42:25 +0000

    Transform toJSON on user to remove password and make the object clearer

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -6,16 +6,28 @@ interface IUser {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password; // remove prop from object
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 // We don't use an arrow function here, becuase it would change the meaning of 'this'
 // In this case, 'this' means the userSchema
