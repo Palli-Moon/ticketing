ebd385f35cf3b85ae04f0cb74359701ab60dc956
Palli Moon <pallimoon@gmail.com>
2023-01-28 20:37:38 +0000

    Add testing dev dependencies

---
+++ ticketing/auth/src/app.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { json } from 'body-parser';
+import { currentUserRouter } from './routes/currentuser';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middleware/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter); // Is there an easier way to do this than importing them one by one and using one by one?
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.all('*', () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,33 +1,6 @@
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-import { json } from 'body-parser';
-import { currentUserRouter } from './routes/currentuser';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middleware/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { app } from './app';
 
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter); // Is there an easier way to do this than importing them one by one and using one by one?
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-app.all('*', () => {
-  throw new NotFoundError();
-});
-app.use(errorHandler);
 mongoose.set('strictQuery', false); // Supress deprecation warning
 
 const start = async () => {
