f4287f86e3cbc09665ff06f44442871f26761b68
Palli Moon <pallimoon@gmail.com>
2023-02-01 22:08:17 +0000

    Move getInitialProps to appcomponent and call page components manually from there

--- ticketing/client/pages/_app.js
+++ ticketing/client/pages/_app.js
@@ -1,8 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
 // Component wrapper. Wraps other pages in this
-const app = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <>
+      <h1>Hello {currentUser.email}</h1>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
-export default app;
+// This is not a page component, so the context here is built like this: { Component, ctx: { req,res }, etc... }
+AppComponent.getInitialProps = async ({ Component, ctx }) => {
+  const { data } = await buildClient(ctx).get('/api/users/currentuser');
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx); // Call getInitialProps manually on page components
+  }
+
+  return { pageProps, ...data };
+};
+
+export default AppComponent;
