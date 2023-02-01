fc8f196d7c3d399ef8a7ad5bfa511bab485bb43a
Palli Moon <pallimoon@gmail.com>
2023-02-01 19:25:17 +0000

    Make signup work with signup service

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -1,14 +1,31 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await axios.post('/api/users/signup', {
+      email,
+      password,
+    });
+
+    console.log(res.data);
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className='form-group'>
         <label>Email Address</label>
-        <input className='form-control' />
+        <input value={email} onChange={(e) => setEmail(e.target.value)} className='form-control' />
       </div>
       <div className='form-group'>
         <label>Password</label>
-        <input type='password' className='form-control' />
+        <input value={password} onChange={(e) => setPassword(e.target.value)} type='password' className='form-control' />
       </div>
       <button className='btn btn-primary'>Sign Up</button>
     </form>
