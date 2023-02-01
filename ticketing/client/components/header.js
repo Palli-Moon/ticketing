58dffdabd0d51823383e8c158c65246330f8ec3d
Palli Moon <pallimoon@gmail.com>
2023-02-01 22:45:55 +0000

    Make header links for signup/in/out

--- ticketing/client/components/header.js
+++ ticketing/client/components/header.js
@@ -1,14 +1,31 @@
 import Link from 'next/link';
 
 export default ({ currentUser }) => {
+  const links = [
+    // This is cool!
+    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className='nav-item'>
+          <Link href={href} className='nav-link'>
+            {label}
+          </Link>
+        </li>
+      );
+    });
+
   return (
     <nav className='navbar navbar-light bg-light'>
       <Link href='/' className='navbar-brand'>
-        Nasty Tickets
+        &nbsp;&nbsp;Nasty Tickets
       </Link>
 
       <div className='d-flex justify-content-end'>
-        <ul className='nav d-flex align-items-center'>{currentUser ? 'Sign Out' : 'Sign in/up'}</ul>
+        <ul className='nav d-flex align-items-center'>{links}</ul>
       </div>
     </nav>
   );
