5e74c9e6a8bfd3de23dd2fc629d6bbfae910b3ac
Palli Moon <pallimoon@gmail.com>
2023-02-01 21:36:34 +0000

    Add buildClient for a reusable api client

---
+++ ticketing/client/api/build-client.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+// Use this convention to access a service in a different namespace:
+// http://NAMEOFSERVICE.NAMESPACE.svc.cluster.local
+// http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+// External name service can be used to shorten this. Will NOT be taught in this course.
+
+export default ({ req }) => {
+  if (typeof window === 'undefined') {
+    // We are on the server
+    // use { headers: { Host: 'hostname' } } to help ingress figure out the hostname. It is also set inside req.headers
+    // req.headers will also forward the cookie
+    return axios.create({
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: req.headers,
+    });
+  } else {
+    // We are on the browser
+    return axios.create({
+      baseURL: '/',
+    });
+  }
+};
--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   // console.log in here would be written in chrome (the browser)
@@ -8,27 +8,10 @@ const LandingPage = ({ currentUser }) => {
 
 // nextjs specific
 // Always executed on the server EXCEPT when navigating from one page to another while in the app
-LandingPage.getInitialProps = async ({ req }) => {
-  // console.log in here would be written in nodejs (on the server)
-
-  // Use this convention to access a service in a different namespace:
-  // http://NAMEOFSERVICE.NAMESPACE.svc.cluster.local
-  // http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
-  // External name service can be used to shorten this. Will NOT be taught in this course.
-
-  if (typeof window === 'undefined') {
-    // We are on the server
-    // use { headers: { Host: 'hostname' } } to help ingress figure out the hostname. It is also set inside req.headers
-    // req.headers will also forward the cookie
-    const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-      headers: req.headers,
-    });
-    return data;
-  } else {
-    // We are on the browser
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  // console.log in here would be written in nodejs (on the server) except if redirected inside the app
+  const { data } = await buildClient(context).get('/api/users/currentuser');
+  return data;
 };
 
 export default LandingPage;
