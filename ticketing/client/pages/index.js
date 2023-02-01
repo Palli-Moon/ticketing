9cbf82959903bde8f335e8cbe00e86d0091b2928
Palli Moon <pallimoon@gmail.com>
2023-02-01 20:18:28 +0000

    Test out getInitialProps

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,3 +1,14 @@
-export default () => {
-  return <h1>Landing page</h1>;
+const LandingPage = ({ color }) => {
+  // console.log in here would be written in chrome (the client)
+  return <h1>Landing page {color}</h1>;
 };
+
+// nextjs specific
+LandingPage.getInitialProps = () => {
+  // console.log in here would be written in nodejs (on the server)
+  console.log('I am on the server');
+
+  return { color: 'red' };
+};
+
+export default LandingPage;
