c203ea792200add79b8190eee5a39f0f164d0a99
Palli Moon <pallimoon@gmail.com>
2023-02-08 19:31:09 +0000

    Setup test suite for tickets

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import cookieSession from 'cookie-session';
 import { json } from 'body-parser';
 import { errorHandler, NotFoundError } from '@ticketingtutorial/common';
+import { createTicketRouter } from './routes/create';
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,6 +14,8 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(createTicketRouter);
 
 app.all('*', () => {
   throw new NotFoundError();
