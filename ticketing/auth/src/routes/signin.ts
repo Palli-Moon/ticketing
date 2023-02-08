659819b87e5e86600d22634c0a999bb4ed310e94
Palli Moon <pallimoon@gmail.com>
2023-02-08 20:48:38 +0000

    Rename User model in auth service to have capital first letter

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 import { validateRequest, BadRequestError } from '@ticketingtutorial/common';
-import { User } from '../models/user';
+import { User } from '../models/User';
 import { Password } from '../utils/password';
 
 const router = express.Router();
