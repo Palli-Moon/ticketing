2298a8db6f8780ba7932e79912febedff7170a3d
Palli Moon <pallimoon@gmail.com>
2023-02-10 20:45:54 +0000

    Add acknowledging on the message

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -10,12 +10,15 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
   console.log('listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created', 'listenerQueueGroup');
+  const options = stan.subscriptionOptions().setManualAckMode(true); // Manual ackmode means the message needs to be acknowledged or the service will keep trying to send it
+  const subscription = stan.subscribe('ticket:created', 'listenerQueueGroup', options);
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
 
     if (typeof data === 'string') {
       console.log(`received event #${msg.getSequence()}, with data: ${data}`);
     }
+
+    msg.ack(); // Acknowledge the message
   });
 });
