7ebb49560dfccb6384e8e2fb4042e4c829242b4f
Palli Moon <pallimoon@gmail.com>
2023-02-14 21:29:06 +0000

    Refactor classes out of listener file

---
+++ ticketing/nats-test/src/events/base-listener.ts
@@ -0,0 +1,38 @@
+import { Message, Stan } from 'node-nats-streaming';
+
+export abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
+  private client: Stan;
+  protected ackWait = 5 * 1000; // 5 sec
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable() // Redelivers every available message when the listener comes back up
+      .setManualAckMode(true) // Manual ackmode means the message needs to be acknowledged or the service will keep trying to send it
+      .setAckWait(this.ackWait) // Sets acknowledgement timeout
+      .setDurableName(this.queueGroupName); // With this and setDeliverAllAvailable only messages that have not been acknowledged will be redelivered. Should be used with queuegroup
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(this.subject, this.queueGroupName, this.subscriptionOptions());
+
+    subscription.on('message', (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+
+      const data = this.parseMessage(msg);
+      this.onMessage(data, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string' ? JSON.parse(data) : JSON.parse(data.toString('utf8'));
+  }
+}
