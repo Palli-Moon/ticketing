95299fc8005d33b99f711c3c32bcf0af45dcdcad
Palli Moon <pallimoon@gmail.com>
2023-02-15 19:02:40 +0000

    Make nats wrapper accessible via getter

--- ticketing/tickets/src/routes/create.ts
+++ ticketing/tickets/src/routes/create.ts
@@ -3,6 +3,7 @@ import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@ticketingtutorial/common';
 import { Ticket } from '../models/Ticket';
 import { TicketCreadetPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -16,12 +17,12 @@ router.post(
     const ticket = new Ticket({ title, price, userId: req.currentUser!.id });
     await ticket.save();
 
-    // new TicketCreadetPublisher(client).publish({
-    //   id: ticket.id,
-    //   title: ticket.title,
-    //   price: ticket.price,
-    //   userId: ticket.userId,
-    // });
+    new TicketCreadetPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }
