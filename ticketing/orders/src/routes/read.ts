ba0e6e01683e8f0dd237d8e097c0799a4b7a56f2
Palli Moon <pallimoon@gmail.com>
2023-02-24 22:46:48 +0000

    Create read specific order route

--- ticketing/orders/src/routes/read.ts
+++ ticketing/orders/src/routes/read.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@ticketingtutorial/common';
+import { NotAuthorizedError, NotFoundError, requireAuth } from '@ticketingtutorial/common';
 import { Order } from '../models/Order';
 
 const router = express.Router();
@@ -10,8 +10,17 @@ router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
   res.send(orders);
 });
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
+router.get('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+  const order = await Order.findById(req.params.orderId).populate('ticket');
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  res.send(order);
 });
 
 export { router as readOrdersRouter };
