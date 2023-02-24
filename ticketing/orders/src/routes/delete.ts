4f3232f29f71c12b2401e61fd9ef7d5f0aeee68d
Palli Moon <pallimoon@gmail.com>
2023-02-24 23:01:11 +0000

    Create delete order route

--- ticketing/orders/src/routes/delete.ts
+++ ticketing/orders/src/routes/delete.ts
@@ -1,9 +1,24 @@
 import express, { Request, Response } from 'express';
+import { Order, OrderStatus } from '../models/Order';
+import { NotAuthorizedError, NotFoundError, requireAuth } from '@ticketingtutorial/common';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
+// We don't actually want to delete the order completely, but rather cancel it
+router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+  const { orderId } = req.params;
+  const order = await Order.findById(orderId);
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+
+  res.status(204).send(order);
 });
 
 export { router as deleteOrdersRouter };
