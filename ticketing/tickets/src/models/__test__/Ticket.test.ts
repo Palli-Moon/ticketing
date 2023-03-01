c09bfd262438d1847f617a68632114c00de24a2e
Palli Moon <pallimoon@gmail.com>
2023-03-01 17:02:14 +0000

    Create OCC test

--- ticketing/tickets/src/models/__test__/Ticket.test.ts
+++ ticketing/tickets/src/models/__test__/Ticket.test.ts
@@ -1,3 +1,27 @@
 import { Ticket } from '../Ticket';
 
-it('implements optimistic concurrency control', async () => {});
+it('implements optimistic concurrency control', async () => {
+  const ticket = Ticket.build({
+    title: 'conbcert',
+    price: 5,
+    userId: '123',
+  });
+
+  await ticket.save();
+
+  // Two instances of the same ticket
+  const ticket1 = await Ticket.findById(ticket.id);
+  const ticket2 = await Ticket.findById(ticket.id);
+
+  ticket1!.set({ price: 10 });
+  ticket2!.set({ price: 15 });
+
+  await ticket1!.save();
+  try {
+    await ticket2!.save();
+  } catch (err) {
+    return;
+  }
+
+  throw new Error('Should not reach this point');
+});
