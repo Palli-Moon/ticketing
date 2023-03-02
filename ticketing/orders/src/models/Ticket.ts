b2da16c4c5320ab20ac54b29f2fc53c7dcfaa5ef
Palli Moon <pallimoon@gmail.com>
2023-03-02 19:16:02 +0000

    Add version to ticket model in orders service and hook it up to listener

--- ticketing/orders/src/models/Ticket.ts
+++ ticketing/orders/src/models/Ticket.ts
@@ -4,6 +4,7 @@
            need all the information about a ticket that the ticket service stores, and so on.
 */
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { Order, OrderStatus } from './Order';
 
 interface TicketAttrs {
@@ -15,6 +16,7 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  version: number;
   isReserved(): Promise<boolean>;
 }
 
@@ -44,6 +46,9 @@ const ticketSchema = new mongoose.Schema(
     },
   }
 );
+
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({
