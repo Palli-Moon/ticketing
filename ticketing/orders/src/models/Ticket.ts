6c13eb4e83ccdcfd48b94d6cdf32c177f0c720d1
Palli Moon <pallimoon@gmail.com>
2023-03-02 20:20:42 +0000

    Add commented out code to show how pre save middleware is made

--- ticketing/orders/src/models/Ticket.ts
+++ ticketing/orders/src/models/Ticket.ts
@@ -51,6 +51,15 @@ const ticketSchema = new mongoose.Schema(
 ticketSchema.set('versionKey', 'version');
 ticketSchema.plugin(updateIfCurrentPlugin);
 
+// How to create pre save middleware
+// ticketSchema.pre('save', function (done) {
+//   this.$where = {
+//     version: this.get('version') - 1,
+//   };
+
+//   done();
+// });
+
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({
     ...attrs,
