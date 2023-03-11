31fea098033276484271ae23432bad1e5e9733e2
Palli Moon <pallimoon@gmail.com>
2023-03-11 15:25:05 +0000

    Add order cancelled listener

--- ticketing/tickets/src/events/listeners/order-cancelled-listener.ts
+++ ticketing/tickets/src/events/listeners/order-cancelled-listener.ts
@@ -1,21 +1,21 @@
 import { Message } from 'node-nats-streaming';
-import { Listener, OrderCreatedEvent, Subjects } from '@ticketingtutorial/common';
+import { Listener, OrderCancelledEvent, Subjects } from '@ticketingtutorial/common';
 import { queueGroupName } from './queue-group-name';
 import { Ticket } from '../../models/Ticket';
 import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 
-export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
-  readonly subject = Subjects.OrderCreated;
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+  async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
     const ticket = await Ticket.findById(data.ticket.id);
 
     if (!ticket) {
       throw new Error('Ticket not found');
     }
 
-    ticket.set({ orderId: data.id });
+    ticket.set({ orderId: undefined }); // optional values work better with undefined than null
     await ticket.save();
     await new TicketUpdatedPublisher(this.client).publish({
       id: ticket.id,
