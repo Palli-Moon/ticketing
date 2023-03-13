b76e2902406e1a63fcdd11004b491f719e397b71
Palli Moon <pallimoon@gmail.com>
2023-03-13 22:59:29 +0000

    Add job on order created event

--- ticketing/expiration/src/events/listeners/order-created-listener.ts
+++ ticketing/expiration/src/events/listeners/order-created-listener.ts
@@ -1,10 +1,14 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@ticketingtutorial/common';
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({ orderId: data.id });
+    msg.ack();
+  }
 }
