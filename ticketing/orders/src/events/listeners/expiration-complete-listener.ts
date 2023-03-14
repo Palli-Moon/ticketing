73ed09ccd247551c1db6617f382ae8724d3e4c24
Palli Moon <pallimoon@gmail.com>
2023-03-14 21:40:42 +0000

    Finish expiration complete listener

--- ticketing/orders/src/events/listeners/expiration-complete-listener.ts
+++ ticketing/orders/src/events/listeners/expiration-complete-listener.ts
@@ -2,13 +2,14 @@ import { Listener, Subjects, ExpirationCompleteEvent } from '@ticketingtutorial/
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
 import { Order, OrderStatus } from '../../models/Order';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
   readonly subject = Subjects.ExpirationComplete;
   queueGroupName = queueGroupName;
 
   async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
-    const order = await Order.findById(data.orderId);
+    const order = await Order.findById(data.orderId).populate('ticket');
 
     if (!order) {
       throw new Error('Order not found');
@@ -17,5 +18,15 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     order.set({
       status: OrderStatus.Cancelled,
     });
+    await order.save();
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
   }
 }
