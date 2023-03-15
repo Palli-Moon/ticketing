9e2105ded0d762b797f356a0f8690b2e4e26ac8e
Palli Moon <pallimoon@gmail.com>
2023-03-15 18:18:03 +0000

    Add tests for post route

---
+++ ticketing/payments/src/routes/__test__/create.test.ts
@@ -0,0 +1,58 @@
+import { OrderStatus } from '@ticketingtutorial/common';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../../app';
+import { Order } from '../../models/Order';
+
+const URI = '/api/payments';
+const COOKIE = global.createCookie;
+
+it('returns a 404 when purchasing an order that does not exist', async () => {
+  await request(app)
+    .post(URI)
+    .set('Cookie', COOKIE())
+    .send({
+      token: 'asdf',
+      orderId: new mongoose.Types.ObjectId().toHexString(),
+    })
+    .expect(404);
+});
+
+it('returns a 401 when purchasing an order that does not belong to the user', async () => {
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+  await request(app)
+    .post(URI)
+    .set('Cookie', COOKIE())
+    .send({
+      token: 'asdf',
+      orderId: order.id,
+    })
+    .expect(401);
+});
+
+it('returns a 400 when purchasing a cancelled order', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Cancelled,
+  });
+  await order.save();
+  await request(app)
+    .post(URI)
+    .set('Cookie', COOKIE(userId))
+    .send({
+      token: 'asdf',
+      orderId: order.id,
+    })
+    .expect(400);
+});
