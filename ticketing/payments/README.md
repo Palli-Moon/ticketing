362a14395ef5a29f053bf4ad67578353bcd5ea95
Palli Moon <pallimoon@gmail.com>
2023-03-16 17:29:29 +0000

    Fix typo and add readme

---
+++ ticketing/payments/README.md
@@ -0,0 +1,1 @@
+`tok_visa` as token will be accepted as a visa card token in a test environment.
--- ticketing/payments/src/routes/create.ts
+++ ticketing/payments/src/routes/create.ts
@@ -25,7 +25,7 @@ router.post(
     }
 
     if (order.status === OrderStatus.Cancelled) {
-      throw new BadRequestError('Cannot pay for an cancelled order');
+      throw new BadRequestError('Cannot pay for a cancelled order');
     }
 
     await stripe.charges.create({
