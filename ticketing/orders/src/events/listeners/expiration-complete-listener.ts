8e5b236bdaaa73dff44d9ac9bf237d2fa23473b3
Palli Moon <pallimoon@gmail.com>
2023-03-19 18:05:55 +0000

    Fix orders being cancelled after being paid for

--- ticketing/orders/src/events/listeners/expiration-complete-listener.ts
+++ ticketing/orders/src/events/listeners/expiration-complete-listener.ts
@@ -14,10 +14,11 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     if (!order) {
       throw new Error('Order not found');
     }
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
+    }
 
-    order.set({
-      status: OrderStatus.Cancelled,
-    });
+    order.set({ status: OrderStatus.Cancelled });
     await order.save();
     await new OrderCancelledPublisher(this.client).publish({
       id: order.id,
