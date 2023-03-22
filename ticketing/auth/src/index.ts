4681fa6778e46f3514067eb8b8a9faa8d6918cc3
Palli Moon <pallimoon@gmail.com>
2023-03-22 21:54:10 +0000

    A little change perchance

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -3,6 +3,8 @@ import { app } from './app';
 
 mongoose.set('strictQuery', false); // Supress deprecation warning
 
+// This is a change
+
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
