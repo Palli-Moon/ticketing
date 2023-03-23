9d7e4e0f6d7c244a0174a8db85e35b7956d15a41
Palli Moon <pallimoon@gmail.com>
2023-03-23 19:15:14 +0000

    Tiny change to trigger deploy

--- ticketing/auth/src/routes/currentuser.ts
+++ ticketing/auth/src/routes/currentuser.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // is there perhaps an easier way than always writing the '/api/users' part?
 router.get('/api/users/currentuser', currentUser, (req, res) => {
-  res.send({ currentUser: req.currentUser || null }); // Current user is null if the user is not set.
+  res.send({ currentUser: req.currentUser || null }); // Current user is null if the user is not set
 });
 
 export { router as currentUserRouter }; // Rename the exported router
